7f15f77ba4b63d521cd8cf29cfc9b5f1c1615cb7
Fladit <zatsarin77@gmail.com>
2022-02-07 00:21:47 +0300

    add router with two main links - MainPage and TodoContainer, which wrapped in React.lazy

--- src/components/TodoContainer/TodoContainer.tsx
+++ src/components/TodoContainer/TodoContainer.tsx
@@ -3,14 +3,13 @@ import {Button} from "@mui/material";
 import TodoList from "../TodoList";
 import {todos} from '../../utils/todos';
 import styles from './TodoContainer.module.scss';
-import classNames from "classnames";
 
-const TodoContainer = ({className}: {className?: string;}) => {
+const TodoContainer = () => {
     const [isTodosVisible, setIsTodosVisible] = useState(false);
     const buttonText = !isTodosVisible? 'Открыть список дел': 'Закрыть список дел';
 
     return (
-        <div className={classNames(styles.root, className)}>
+        <div className={styles.root}>
             <Button onClick={() => {setIsTodosVisible((isVisible => !isVisible));}}>{buttonText}</Button>
             {isTodosVisible && <TodoList todos={todos}/>}
         </div>
